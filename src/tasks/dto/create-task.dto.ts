0a29798a1686d28e995da6f992a355778a32d480
ivansorroche <ivan.sorroche@hotmail.com>
2025-07-30 15:42:27 -0300

    [master][install and apply decoratorns on DTO]

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,8 +1,17 @@
 
 // DTO > Data Transfer Object ( Objeto de transferencia de dados)
 
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+  @IsString()
+  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   readonly description: string;
   // completed?: boolean; // O ponto de interrogação indica que este campo é opcional
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,17 @@
+import { IsBoolean, IsOptional, IsString, MinLength } from "class-validator";
+
 export class UpdateTaskDto {
+  @IsString()
+  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+  @IsOptional()
   readonly name?: string;
+
+  @IsString()
+  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+  @IsOptional()
   readonly description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   readonly completed?: boolean; // O ponto de interrogação indica que este campo é opcional
 }
