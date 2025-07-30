666d39cf34b5d0c93dda008ada2d85fbecf774c5
ivansorroche <ivan.sorroche@hotmail.com>
2025-07-29 22:27:28 -0300

    [master][add new item and findById]

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,7 +6,30 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) { }
 
   @Get()
-  getAllTasks() {
+  findAll() {
     return this.tasksService.listAllTasks()
+  }
+
+  @Get(':id')
+  getTaskById(@Param("id") id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() body: any) {
+    console.log(body)
+    return this.tasksService.create(body);
+  }
+
+  @Patch(':id')
+  updateTask(@Param("id") id: string, @Body() body: any) {
+    console.log(id)
+    console.log(body)
+    return "atualizando a task de id " + id + " com os dados: " + JSON.stringify(body);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param("id") id: string) {
+    return "Deletando a task de id " + id;
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
 
+  private tasks: Task[] = [
+    { id: 1, name: 'Task One', description: 'This is the first task', completed: false },
+    { id: 2, name: 'Task Two', description: 'This is the second task', completed: true },
+  ];
+
   listAllTasks() {
-    return [
-      { id: 1, title: 'Task One', description: 'This is the first task' },
-    ]
+    return this.tasks
   }
+
+  getTaskById(id: string) {
+    return this.tasks.find(task => task.id === Number(id));
+  }
+
+  create(body: any) {
+    const newId = this.tasks.length + 1;
+    const newTask: Task = {
+      id: newId,
+      ...body,
+    }
+
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
 }
