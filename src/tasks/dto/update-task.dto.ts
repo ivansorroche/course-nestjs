1193fcc912f34e586b12a3d0a2377f14b5b8df7d
ivansorroche <ivan.sorroche@hotmail.com>
2025-07-30 15:47:32 -0300

    [master][extends anothers DTO with partial types]

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,16 +1,24 @@
-import { IsBoolean, IsOptional, IsString, MinLength } from "class-validator";
+import { PartialType } from "@nestjs/mapped-types";
+import { IsBoolean, IsOptional } from "class-validator";
+import { CreateTaskDto } from "./create-task.dto";
 
-export class UpdateTaskDto {
-  @IsString()
-  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
-  @IsOptional()
-  readonly name?: string;
+// export class UpdateTaskDto {
+//   @IsString()
+//   @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+//   @IsOptional()
+//   readonly name?: string;
 
-  @IsString()
-  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
-  @IsOptional()
-  readonly description?: string;
+//   @IsString()
+//   @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+//   @IsOptional()
+//   readonly description?: string;
 
+//   @IsBoolean()
+//   @IsOptional()
+//   readonly completed?: boolean; // O ponto de interrogação indica que este campo é opcional
+// }
+
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsBoolean()
   @IsOptional()
   readonly completed?: boolean; // O ponto de interrogação indica que este campo é opcional
